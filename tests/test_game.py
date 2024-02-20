59c3d82d47d3a57fc5feaa07c8c56c71beed305b
Thomas Cadet <thomas.cadet@gmail.com>
2024-02-20 19:33:54 +0000

    TDD: Check that attempt exists in the english dctionary

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,7 @@
 
 from longest_word.game import Game
 import string
+import pytest
 
 # tests/test_game.py
 class TestGame:
@@ -23,5 +24,10 @@ class TestGame:
 
     def test_is_no_input(self):
         game = Game()
-
         assert game.is_valid('') == False
+
+    def test_unknown_word_is_invalid(self):
+        """A word that is not in the english directory should no be valid"""
+        new_game = Game()
+        new_game.grid = 'KWIENFUQW' # Force the grid to a test case:
+        assert new_game.is_valid('KWIENFUQW') is False
